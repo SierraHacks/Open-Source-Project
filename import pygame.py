c8881a8ad2fa51bdb9542f8dd6f02c1da79054cf
Sanjay Krishna <144625268+SierraHacks@users.noreply.github.com>
2023-12-10 14:19:02 -0800

    Background

--- import pygame.py
+++ import pygame.py
@@ -1,3 +1,10 @@
 import pygame
+import sys
 pygame.init()
 screen=pygame.display.pygame.display.set_mode([100, 100])
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+bg = pygame.image.load("image0")
