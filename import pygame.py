62e60d5329b5c00c70b4830c2dc6e71286d0f7e4
Sanjay Krishna <ss1967@pleasantonusd.net>
2023-12-10 14:09:06 -0800

    Background

--- import pygame.py
+++ import pygame.py
@@ -1,3 +1,9 @@
 import pygame
 pygame.init()
 screen=pygame.display.pygame.display.set_mode([100, 100])
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+bg = pygame.image.load("image0")
